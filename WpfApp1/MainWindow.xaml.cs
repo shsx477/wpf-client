cb749a0968989ad1d850619d387c14f2917bce33
shsx477 <shsx477@gmail.com>
2020-04-12 05:22:31 +0900

    create solution

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using MahApps.Metro.Controls;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
